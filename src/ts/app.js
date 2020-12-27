649060d67cd20d0e373d7d78d195818ff04425ef
vitalij bakalov <bakalov.vitaliy@gmail.com>
2020-12-27 20:19:43 +0700

    Произведена настройка webpack

---
+++ src/ts/app.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                test text
+            </div>
+        )
+    }
+}
+
+const destination = document.querySelector('#root');
+
+ReactDOM.render(
+    <div>
+        <App />
+    </div>,
+    destination
+);
