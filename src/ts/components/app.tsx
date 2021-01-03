b8a9393a164862fbd854ec0e9607f23682983ed6
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-03 16:05:08 +0700

    Добавлен компонент servicesScreen

--- src/ts/components/app.tsx
+++ src/ts/components/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AboutScreen from './aboutScreen';
 import SkillsScreen from './skillsScreen';
+import ServicesScreen from './servicesScreen'
 
 class App extends React.Component {
     render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
             <div>
                 <AboutScreen />
                 <SkillsScreen />
+                <ServicesScreen />
             </div>
         );
     }
---
+++ src/ts/components/servicesScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class ServicesScreen extends React.Component {
+    render() {
+        return (
+            <div className='servicesScreen'>
+            </div>
+        );
+    }
+}
+
+export default ServicesScreen;
