b0a933676181498f00df025d9f64bc9fde368c63
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-03 17:29:49 +0700

    Добавлен компонент header

--- src/ts/components/aboutScreen.tsx
+++ src/ts/components/aboutScreen.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import '../../scss/aboutScreen.scss'
+import React from 'react';
+import Header from './header';
+import '../../scss/aboutScreen.scss';
 
 class AboutScreen extends React.Component {
     render() {
         return (
             <div className='aboutScreen'>
+                <Header />
             </div>
         );
     }
---
+++ src/ts/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <footer className='header'>
+            </footer>
+        );
+    }
+}
+
+export default Header;
