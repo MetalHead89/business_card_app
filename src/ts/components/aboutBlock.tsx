b06c49712aadfb814e9634b2790164a6d33be5c0
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2021-01-04 10:16:42 +0700

    Добавлен компонент aboutBlock

---
+++ src/ts/components/aboutBlock.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutBlock extends React.Component {
+    render() {
+        return (
+            <div className='aboutBlock'>
+                <div className='aboutBlock__text'></div>
+                <div className='aboutBlock__photo'></div>
+            </div>
+        );
+    }
+}
+
+export default AboutBlock;
--- src/ts/components/aboutScreen.tsx
+++ src/ts/components/aboutScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './header';
+import AboutBlock from './aboutBlock';
 import '../../scss/aboutScreen.scss';
 
 class AboutScreen extends React.Component {
@@ -7,6 +8,7 @@ class AboutScreen extends React.Component {
         return (
             <div className='aboutScreen'>
                 <Header />
+                <AboutBlock />
             </div>
         );
     }
