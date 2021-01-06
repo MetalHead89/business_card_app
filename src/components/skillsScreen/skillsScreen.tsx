868679d8b47feffbc7212923f5e558374aa98e7c
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-06 16:54:00 +0700

    Добавлены новые навыки в scillsScreen

--- src/components/skillsScreen/skillsScreen.tsx
+++ src/components/skillsScreen/skillsScreen.tsx
@@ -5,8 +5,13 @@ import SkillBlock from "../skillBlock/skillsBlock";
 class SkillsScreen extends React.Component {
   render() {
     return (
-      <div className='skillsScreen'>
-        <SkillBlock style='fab fa-react'/>
+      <div className="skillsScreen">
+        <SkillBlock style="fab fa-react" />
+        <SkillBlock style="fab fa-js" />
+        <SkillBlock style="fab fa-git" />
+        <SkillBlock style="fab fa-html5" />
+        <SkillBlock style="fab fa-css3-alt" />
+        <SkillBlock style="fab fa-sass" />
       </div>
     );
   }
