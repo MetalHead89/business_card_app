96946dfa1acac4ebb17bbd6045e23ff3aeaf7010
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-06 16:37:17 +0700

    Добавлен компонент skillBlock

---
+++ src/components/skillBlock/skillsBlock.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./skillBlock.scss";
+import { IProps, IState } from "../../ts/interfaces";
+
+class SkillBlock extends React.Component<IProps, IState> {
+  render() {
+    return (
+      <div className="skillBlock">
+        <i className={`skillBlock__icon ${this.props.style}`}></i>
+        <div>Rating</div>
+      </div>
+    );
+  }
+}
+
+export default SkillBlock;
--- src/components/skillsScreen/skillsScreen.tsx
+++ src/components/skillsScreen/skillsScreen.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import './skillsScreen.scss'
+import React from "react";
+import "./skillsScreen.scss";
+import SkillBlock from "../skillBlock/skillsBlock";
 
 class SkillsScreen extends React.Component {
-    render() {
-        return (
-            <div className='skillsScreen'>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='skillsScreen'>
+        <SkillBlock style='fab fa-react'/>
+      </div>
+    );
+  }
 }
 
 export default SkillsScreen;
