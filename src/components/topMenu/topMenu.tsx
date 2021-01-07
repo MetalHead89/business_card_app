2b5ddcd1705f29e9d491f3308eef5fede074c011
Bakalov Vitalij <bakalov.vitaliy@gmail.com>
2021-01-08 05:29:11 +0700

    Данные меню вынесены в отдельный массив

--- src/components/topMenu/topMenu.tsx
+++ src/components/topMenu/topMenu.tsx
@@ -2,24 +2,34 @@ import React from "react";
 import "./topMenu.scss";
 import { BrowserRouter, NavLink } from "react-router-dom";
 
+const menuData = [
+  { path: '/about', linkText: 'Обо мне' },
+  { path: '/skills', linkText: 'Навыки' },
+  { path: '/services', linkText: 'Услуги' },
+  { path: '/contacts', linkText: 'Контакты' }
+];
+
+// const menuItems = menuData.map(item => {return (
+// `< li className="topMenu__menu-item" >
+//   <NavLink to=${item.path}>${item.linkText}</NavLink>
+// </li >`
+// }))
+
+const menuItems = menuData.map(item => {
+  return (
+    < li className='topMenu__menu-item' >
+      <NavLink to={item.path}>{item.linkText}</NavLink>
+    </li >
+  )
+})
+
 class TopMenu extends React.Component {
   render() {
     return (
       <nav className="topMenu">
         <BrowserRouter>
           <ul className="topMenu__menu">
-            <li className="topMenu__menu-item">
-              <NavLink to="/about">Обо мне</NavLink>
-            </li>
-            <li className="topMenu__menu-item">
-              <NavLink to="/skills">Навыки</NavLink>
-            </li>
-            <li className="topMenu__menu-item">
-              <NavLink to="/services">Заказы</NavLink>
-            </li>
-            <li className="topMenu__menu-item">
-              <NavLink to="/contacts">Контакты</NavLink>
-            </li>
+            {menuItems}
           </ul>
         </BrowserRouter>
       </nav>
