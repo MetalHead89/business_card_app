5e895ad870be3cb0dc1abcb4bdf5a8b4580ae4d2
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-07 20:56:25 +0700

    Добавлен react-router-dom в проект и NavLinks в пункты topMenu

--- src/components/topMenu/topMenu.tsx
+++ src/components/topMenu/topMenu.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import './topMenu.scss'
+import React from "react";
+import "./topMenu.scss";
+import { BrowserRouter, NavLink } from "react-router-dom";
 
 class TopMenu extends React.Component {
-    render() {
-        return (
-            <nav className='topMenu'>
-                <ul className='topMenu__menu'>
-                    <li className='topMenu__menu-item'>Обо мне</li>
-                    <li className='topMenu__menu-item'>Навыки</li>
-                    <li className='topMenu__menu-item'>Заказы</li>
-                    <li className='topMenu__menu-item'>Контакты</li>
-                </ul>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className="topMenu">
+        <BrowserRouter>
+          <ul className="topMenu__menu">
+            <li className="topMenu__menu-item">
+              <NavLink to="/about">Обо мне</NavLink>
+            </li>
+            <li className="topMenu__menu-item">
+              <NavLink to="/skills">Навыки</NavLink>
+            </li>
+            <li className="topMenu__menu-item">
+              <NavLink to="/services">Заказы</NavLink>
+            </li>
+            <li className="topMenu__menu-item">
+              <NavLink to="/contacts">Контакты</NavLink>
+            </li>
+          </ul>
+        </BrowserRouter>
+      </nav>
+    );
+  }
 }
 
 export default TopMenu;
