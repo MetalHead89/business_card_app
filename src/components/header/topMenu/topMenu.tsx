2c20ce517df5e9e45e05819257acad115d53c363
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-01-09 23:59:34 +0700

    Проведено структурирование кода компонента TopMenu

--- src/components/header/topMenu/topMenu.tsx
+++ src/components/header/topMenu/topMenu.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./topMenu.scss";
-import { BrowserRouter, NavLink } from "react-router-dom";
+import React from 'react';
+import './topMenu.scss';
+import { BrowserRouter, NavLink } from 'react-router-dom';
 
 interface IMenuItem {
   [key: string]: string;
@@ -17,19 +17,17 @@ interface IState {
 class TopMenu extends React.Component<IProps, IState> {
   menuItems = this.props.menuItems.map((item, i) => {
     return (
-      < li key={i} className='topMenu__menu-item' >
+      <li key={i} className="topMenu__menu-item">
         <NavLink to={item.path}>{item.linkText}</NavLink>
-      </li >
-    )
-  })
+      </li>
+    );
+  });
 
   render() {
     return (
       <nav className="topMenu">
         <BrowserRouter>
-          <ul className="topMenu__menu">
-            {this.menuItems}
-          </ul>
+          <ul className="topMenu__menu">{this.menuItems}</ul>
         </BrowserRouter>
       </nav>
     );
