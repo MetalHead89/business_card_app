6ce4b24321eac5d59e2bb14e78504abaf5834520
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-01-11 09:47:18 +0700

    Удалён BrowserRouter из topMenu

--- src/components/header/topMenu/topMenu.tsx
+++ src/components/header/topMenu/topMenu.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './topMenu.scss';
-import { BrowserRouter, NavLink } from 'react-router-dom';
 
 interface IMenuItem {
   [key: string]: string;
@@ -18,9 +17,9 @@ class TopMenu extends React.Component<IProps, IState> {
   menuItems = this.props.menuItems.map((item, i) => {
     return (
       <li key={i} className="topMenu__menu-item">
-        <NavLink to={item.anchor} onClick={() => window.location.hash=`#${item.anchor}`}>
+        <a onClick={() => window.location.hash=`#${item.anchor}`}>
           {item.linkText}
-        </NavLink>
+        </a>
       </li>
     );
   });
@@ -28,9 +27,7 @@ class TopMenu extends React.Component<IProps, IState> {
   render() {
     return (
       <nav className="topMenu">
-        <BrowserRouter>
           <ul className="topMenu__menu">{this.menuItems}</ul>
-        </BrowserRouter>
       </nav>
     );
   }
