8fe2ba74b853575e17e96c020c6ab97a289bbfac
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-03-21 19:22:59 +0700

    Компонент SkillBlock сделан функциональным

--- src/components/skillsScreen/skillBlock/skillBlock.tsx
+++ src/components/skillsScreen/skillBlock/skillBlock.tsx
@@ -5,19 +5,13 @@ export interface IProps {
   [key: string]: string;
 }
 
-export interface IState {
-  [key: string]: string;
-}
-
-class SkillBlock extends React.Component<IProps, IState> {
-  render() {
-    return (
-      <div className="skillBlock">
-        <i className={`skillBlock__icon ${this.props.iconClass}`}></i>
-        <div>Rating</div>
-      </div>
-    );
-  }
-}
+const SkillBlock = (props: IProps): JSX.Element => {
+  return (
+    <div className="skillBlock">
+      <i className={`skillBlock__icon ${props.iconClass}`}></i>
+      <div>Rating</div>
+    </div>
+  );
+};
 
 export default SkillBlock;
