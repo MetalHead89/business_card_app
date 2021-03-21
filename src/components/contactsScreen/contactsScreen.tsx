640698366b66f33886c2f233c19f237c7ebbc5dc
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-03-21 19:13:30 +0700

    Компонент ContactsScreen сделан функциональным

--- src/components/contactsScreen/contactsScreen.tsx
+++ src/components/contactsScreen/contactsScreen.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './contactsScreen.scss';
 
-class ContactsScreen extends React.Component {
-  render() {
-    return <div id="contacts" className="contactsScreen"></div>;
-  }
-}
+const ContactsScreen = (): JSX.Element => {
+  return <div id="contacts" className="contactsScreen"></div>;
+};
 
 export default ContactsScreen;
