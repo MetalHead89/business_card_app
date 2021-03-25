11abed3a5c0fdb34e1279b7fe2c1d4b0e170c103
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-03-25 15:53:27 +0700

    Добавлены дополнительные свойства в объект state

--- src/ts/redux/state.ts
+++ src/ts/redux/state.ts
@@ -2,40 +2,44 @@ let reRenderApp: any = null;
 
 const state = {
   header: {
-    menuItems: [
-      { id: 1, anchor: 'about', linkText: 'Обо мне', active: true },
-      { id: 2, anchor: 'skills', linkText: 'Навыки', active: false },
-      { id: 3, anchor: 'services', linkText: 'Услуги', active: false },
-      { id: 4, anchor: 'contacts', linkText: 'Контакты', active: false },
-    ],
+    topMenu: {
+      menuItems: [
+        { id: 1, anchor: 'about', linkText: 'Обо мне', active: true },
+        { id: 2, anchor: 'skills', linkText: 'Навыки', active: false },
+        { id: 3, anchor: 'services', linkText: 'Услуги', active: false },
+        { id: 4, anchor: 'contacts', linkText: 'Контакты', active: false },
+      ],
+    },
   },
   aboutScreen: {
-    text: [
-      'Противоположная точка зрения подразумевает, что элементы политического процесса ' +
-        'набирают популярность среди определенных слоев населения, а значит, должны быть ' +
-        'ассоциативно распределены по отраслям.',
-      'Также как разбавленное изрядной долей эмпатии, ' +
-        'рациональное мышление в значительной степени обусловливает важность новых принципов ' +
-        'формирования материально-технической и кадровой базы. Мы вынуждены отталкиваться ' +
-        'от того, что граница обучения кадров предполагает независимые способы реализации ' +
-        'инновационных методов управления процессами.',
-    ],
-    photoLink: '/src/images/photo.png',
+    aboutBlock: {
+      text: [
+        'Противоположная точка зрения подразумевает, что элементы политического процесса ' +
+          'набирают популярность среди определенных слоев населения, а значит, должны быть ' +
+          'ассоциативно распределены по отраслям.',
+        'Также как разбавленное изрядной долей эмпатии, ' +
+          'рациональное мышление в значительной степени обусловливает важность новых принципов ' +
+          'формирования материально-технической и кадровой базы. Мы вынуждены отталкиваться ' +
+          'от того, что граница обучения кадров предполагает независимые способы реализации ' +
+          'инновационных методов управления процессами.',
+      ],
+      photoLink: '/src/images/photo.png',
+    },
   },
   skillsScreen: {
-    iconClasses: [
-      'fab fa-react',
-      'fab fa-js',
-      'fab fa-git',
-      'fab fa-html5',
-      'fab fa-css3-alt',
-      'fab fa-sass',
+    skills: [
+      { icon: 'fab fa-react' },
+      { icon: 'fab fa-js' },
+      { icon: 'fab fa-git' },
+      { icon: 'fab fa-html5' },
+      { icon: 'fab fa-css3-alt' },
+      { icon: 'fab fa-sass' },
     ],
   },
 };
 
 const selectMenuItem = (id: number): void => {
-  state.header.menuItems.forEach((item) => {
+  state.header.topMenu.menuItems.forEach((item) => {
     item.active = item.id === id ? true : false;
   });
 
