f8a6e74a550f942b792f7bf81c4c0007d3e478cc
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-03-25 20:14:39 +0700

    Добавлен объект store в state.js

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -3,19 +3,18 @@ import ReactDOM from 'react-dom';
 import App from '../components/app';
 import '../scss/main.scss';
 import { IState } from './interfaces';
-import state, { subscribe } from './redux/state';
-import { selectMenuItem } from './redux/state';
+import store from './redux/state';
 
 const reRenderApp = (state: IState): void => {
   const destination = document.querySelector('#root');
 
   if (destination != null) {
     ReactDOM.render(
-      <App state={state} selectMenuItem={selectMenuItem} />,
-      destination,
+      <App state={state} selectMenuItem={store.selectMenuItem.bind(store)} />,
+      destination
     );
   }
 };
 
-subscribe(reRenderApp);
-reRenderApp(state);
+store.subscribe(reRenderApp);
+reRenderApp(store.getState());
--- src/ts/redux/state.ts
+++ src/ts/redux/state.ts
@@ -1,54 +1,59 @@
-let reRenderApp: any = null;
+import { IState } from '../interfaces';
 
-const state = {
-  header: {
-    topMenu: {
-      menuItems: [
-        { id: 1, anchor: 'about', linkText: 'Обо мне', active: true },
-        { id: 2, anchor: 'skills', linkText: 'Навыки', active: false },
-        { id: 3, anchor: 'services', linkText: 'Услуги', active: false },
-        { id: 4, anchor: 'contacts', linkText: 'Контакты', active: false },
+const store = {
+  _state: {
+    header: {
+      topMenu: {
+        menuItems: [
+          { id: 1, anchor: 'about', linkText: 'Обо мне', active: true },
+          { id: 2, anchor: 'skills', linkText: 'Навыки', active: false },
+          { id: 3, anchor: 'services', linkText: 'Услуги', active: false },
+          { id: 4, anchor: 'contacts', linkText: 'Контакты', active: false },
+        ],
+      },
+    },
+    aboutScreen: {
+      aboutBlock: {
+        text: [
+          'Противоположная точка зрения подразумевает, что элементы политического процесса ' +
+            'набирают популярность среди определенных слоев населения, а значит, должны быть ' +
+            'ассоциативно распределены по отраслям.',
+          'Также как разбавленное изрядной долей эмпатии, ' +
+            'рациональное мышление в значительной степени обусловливает важность новых принципов ' +
+            'формирования материально-технической и кадровой базы. Мы вынуждены отталкиваться ' +
+            'от того, что граница обучения кадров предполагает независимые способы реализации ' +
+            'инновационных методов управления процессами.',
+        ],
+        photoLink: '/src/images/photo.png',
+      },
+    },
+    skillsScreen: {
+      skills: [
+        { icon: 'fab fa-react' },
+        { icon: 'fab fa-js' },
+        { icon: 'fab fa-git' },
+        { icon: 'fab fa-html5' },
+        { icon: 'fab fa-css3-alt' },
+        { icon: 'fab fa-sass' },
       ],
     },
   },
-  aboutScreen: {
-    aboutBlock: {
-      text: [
-        'Противоположная точка зрения подразумевает, что элементы политического процесса ' +
-          'набирают популярность среди определенных слоев населения, а значит, должны быть ' +
-          'ассоциативно распределены по отраслям.',
-        'Также как разбавленное изрядной долей эмпатии, ' +
-          'рациональное мышление в значительной степени обусловливает важность новых принципов ' +
-          'формирования материально-технической и кадровой базы. Мы вынуждены отталкиваться ' +
-          'от того, что граница обучения кадров предполагает независимые способы реализации ' +
-          'инновационных методов управления процессами.',
-      ],
-      photoLink: '/src/images/photo.png',
-    },
+  getState(): IState {
+    return this._state;
   },
-  skillsScreen: {
-    skills: [
-      { icon: 'fab fa-react' },
-      { icon: 'fab fa-js' },
-      { icon: 'fab fa-git' },
-      { icon: 'fab fa-html5' },
-      { icon: 'fab fa-css3-alt' },
-      { icon: 'fab fa-sass' },
-    ],
+  _callSubscriber(arg: any): void {
+    console.log('no subscribers');
+  },
+  selectMenuItem(id: number): void {
+    this._state.header.topMenu.menuItems.forEach((item) => {
+      item.active = item.id === id ? true : false;
+    });
+
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer: any): void {
+    this._callSubscriber = observer;
   },
 };
 
-const selectMenuItem = (id: number): void => {
-  state.header.topMenu.menuItems.forEach((item) => {
-    item.active = item.id === id ? true : false;
-  });
-
-  reRenderApp(state);
-};
-
-const subscribe = (observer: any): void => {
-  reRenderApp = observer;
-};
-
-export default state;
-export { selectMenuItem, subscribe };
+export default store;
