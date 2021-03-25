837514523d81f7411e6201e1e3ee9f0a3164cc2e
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-03-25 15:49:27 +0700

    Удалён лишний код из index.tsx

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -2,26 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/app';
 import '../scss/main.scss';
+import { IState } from './interfaces';
 import state, { subscribe } from './redux/state';
 import { selectMenuItem } from './redux/state';
-
-interface IState {
-  header: {
-    menuItems: {
-      id: number;
-      anchor: string;
-      linkText: string;
-      active: boolean;
-    }[];
-  };
-  aboutScreen: {
-    text: string[];
-    photoLink: string;
-  };
-  skillsScreen: {
-    iconClasses: string[];
-  };
-}
 
 const reRenderApp = (state: IState): void => {
   const destination = document.querySelector('#root');
@@ -29,7 +12,7 @@ const reRenderApp = (state: IState): void => {
   if (destination != null) {
     ReactDOM.render(
       <App state={state} selectMenuItem={selectMenuItem} />,
-      destination
+      destination,
     );
   }
 };
