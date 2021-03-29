f61bd77aee743b87b88b17fd65bbec54c44fd604
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-03-29 09:05:38 +0700

    Изменён headerReducer, добавлено начальное состояние

--- src/ts/redux/header-reducer.tsx
+++ src/ts/redux/header-reducer.tsx
@@ -1,13 +1,26 @@
 import { IAction, IHeaderState } from '../interfaces';
 
 const CLICK_TO_MENU_ITEM = 'CLICK-TO-MENU-ITEM';
+const initialState = {
+  topMenu: {
+    menuItems: [
+      { id: 1, anchor: 'about', linkText: 'Обо мне', active: true },
+      { id: 2, anchor: 'skills', linkText: 'Навыки', active: false },
+      { id: 3, anchor: 'services', linkText: 'Услуги', active: false },
+      { id: 4, anchor: 'contacts', linkText: 'Контакты', active: false },
+    ],
+  },
+};
 
 const clickToMenuItemActionCreator = (id: number): IAction => ({
   type: CLICK_TO_MENU_ITEM,
   args: { id },
 });
 
-const headerReducer = (state: IHeaderState, action: IAction): IHeaderState => {
+const headerReducer = (
+  state: IHeaderState = initialState,
+  action: IAction,
+): IHeaderState => {
   switch (action.type) {
     case CLICK_TO_MENU_ITEM:
       state.topMenu.menuItems.forEach((item) => {
