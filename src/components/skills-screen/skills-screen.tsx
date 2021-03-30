63d7c801f002b3c6ec183295de4fd11e6892cdaa
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-03-30 16:35:09 +0700

    Удалён лишний код из SkillsScreen

--- src/components/skills-screen/skills-screen.tsx
+++ src/components/skills-screen/skills-screen.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { ISkills } from '../../ts/interfaces';
+import { ISkillsScreenState } from '../../ts/interfaces';
 import './skills-screen.scss';
 import SkillBlock from './skill-block/skill-block';
 
-const SkillsScreen = (props: ISkills): JSX.Element => {
-  const iconClasses = props.skills.map((skill) => {
+const SkillsScreen = (props: ISkillsScreenState): JSX.Element => {
+  const skills = props.skills.map((skill) => {
     return <SkillBlock icon={skill.icon} />;
   });
 
   return (
     <div id="skills" className="skills-screen">
-      <div className="skills-screen__skills">{iconClasses}</div>
+      <div className="skills-screen__skills">{skills}</div>
     </div>
   );
 };
