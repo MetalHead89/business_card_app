533c3d946892837e4b5034941cab68e073a9f079
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-03-30 09:23:27 +0700

    В redux-store добавлен appReducer

--- src/ts/redux/redux-store.ts
+++ src/ts/redux/redux-store.ts
@@ -2,9 +2,11 @@ import { combineReducers, createStore } from 'redux';
 import headerReducer from './header-reducer';
 import aboutScreenReducer from './about-screen-reducer';
 import skillsScreenReducer from './skills-screen-reducer';
+import appReducer from './app-reducer';
 
-// Собирает переданные в параметрах reducers в один reducer
+// Собирает переданные в параметрах reducers в один объект
 const reducers = combineReducers({
+  app: appReducer,
   header: headerReducer,
   aboutScreen: aboutScreenReducer,
   skillsScreen: skillsScreenReducer,
