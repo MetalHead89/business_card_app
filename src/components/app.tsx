b952ca25220e5bc2f60bc7fddf15127acfa81d9d
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-03-30 16:39:45 +0700

    App больше не использует state приложения, теперь он работает с объектом store

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import AboutScreen from './about-screen/about-screen';
-import SkillsScreen from './skills-screen/skills-screen';
 import ServicesScreen from './services-screen/services-screen';
 import Header from './header/header';
 import ContactsScreen from './contacts-screen/contacts-screen';
-import { IApp } from '../ts/interfaces';
+import { IStore } from '../ts/interfaces';
+import SkillsScreenContainer from './skills-screen/skills-screen-container';
 
-const App = (props: IApp): JSX.Element => {
+const App = (props: IStore): JSX.Element => {
   return (
     <div className="app-body">
-      <Header
-        topMenu={{
-          menuItems: props.state.header.topMenu.menuItems,
-          dispatch: props.dispatch,
-        }}
-      />
-      <AboutScreen
-        aboutBlock={{
-          text: props.state.aboutScreen.aboutBlock.text,
-          photoLink: props.state.aboutScreen.aboutBlock.photoLink,
-        }}
-      />
-      <SkillsScreen skills={props.state.skillsScreen.skills} />
+      <Header store={props.store} />
+      <AboutScreen store={props.store} />
+      <SkillsScreenContainer store={props.store} />
       <ServicesScreen />
       <ContactsScreen />
     </div>
