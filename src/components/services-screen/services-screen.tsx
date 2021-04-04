e1f4db4fca863d2ecd3b587fa67f845f822b39d8
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-04-04 16:57:55 +0700

    Проведен рефакторинг компонента ServicesScreen

--- src/components/services-screen/services-screen.tsx
+++ src/components/services-screen/services-screen.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { IServicesScreenState } from '../../ts/interfaces';
+import ServiceBlock from './service-block/service-block';
 import './services-screen.scss';
 
-const ServicesScreen = (): JSX.Element => {
-  return <div id="services" className="services-screen"></div>;
+const ServicesScreen = (props: IServicesScreenState): JSX.Element => {
+  const services = props.services.map((service) => {
+    return (
+      <ServiceBlock
+        id={service.id}
+        icon={service.icon}
+        title={service.title}
+        description={service.description}
+      />
+    );
+  });
+
+  return (
+    <div id="services" className="services-screen">
+      <div className="services-screen__services">{services}</div>
+    </div>
+  );
 };
 
 export default ServicesScreen;
