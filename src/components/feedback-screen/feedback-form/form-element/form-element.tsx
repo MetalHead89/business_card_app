2125b1356af9cef68580f8dd39cba7e24fc6e187
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-04-06 16:39:10 +0700

    Проведен рефакторинг компонента FormElement

--- src/components/feedback-screen/feedback-form/form-element/form-element.tsx
+++ src/components/feedback-screen/feedback-form/form-element/form-element.tsx
@@ -2,24 +2,28 @@ import React from 'react';
 import { IFormElement } from '../../../../ts/interfaces';
 
 const FormElement = (props: IFormElement): JSX.Element => {
-  let control = <input />;
-
-  if (props.type === 'text' || props.type === 'textarea') {
-    control = (
-      <label>
-        {props.label}
-        <input className="text-field" type={props.type} />
-      </label>
-    );
-  } else if (props.type === 'submit') {
-    control = (
-      <input className="submit" type={props.type}>
-        {props.text}
-      </input>
-    );
+  switch (props.type) {
+    case 'textarea':
+      return (
+        <label>
+          {props.label}
+          <textarea className="text-field" />
+        </label>
+      );
+    case 'submit':
+      return (
+        <button className="submit" type={props.type}>
+          {props.text}
+        </button>
+      );
+    default:
+      return (
+        <label>
+          {props.label}
+          <input className="text-field" type={props.type} />
+        </label>
+      );
   }
-
-  return control;
 };
 
 export default FormElement;
