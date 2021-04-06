fc1d0d0b26e75faccee44d338af27719a9c72524
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-04-06 15:18:28 +0700

    Изменен FeedbackScreen

--- src/components/feedback-screen/feedback-screen.tsx
+++ src/components/feedback-screen/feedback-screen.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { IFeedbackScreenState } from '../../ts/interfaces';
+import FeedbackForm from './feedback-form/feedback-form';
 import './feedback-screen.scss';
 
-const FeedbackScreen = (): JSX.Element => {
-  return <div id="contacts" className="feedback-screen"></div>;
+const FeedbackScreen = (props: IFeedbackScreenState): JSX.Element => {
+  return (
+    <div id="contacts" className="feedback-screen">
+      <FeedbackForm {...props.feedbackForm}></FeedbackForm>
+    </div>
+  );
 };
 
 export default FeedbackScreen;
