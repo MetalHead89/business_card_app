b98e650fe0c3cfcfa5789b14049417c0bfdf2f7d
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-04-08 14:33:06 +0700

    Добавлен компонент PortfolioScreen

---
+++ src/components/portfolio-screen/portfolio-screen.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { IPortfolioScreenState } from '../../ts/interfaces';
+
+const portfolioScreen = (props: IPortfolioScreenState): JSX.Element => {
+  const currentSlide = props.slider.slides.filter((slide) => slide.active);
+
+  return (
+    <div className="portfolio-slider">
+      <div className="portfolio-slider__image-section">
+        <img
+          className="portfolio-slider__image"
+          src={currentSlide[0].imageSection.image}
+          alt="slide"
+        />
+      </div>
+      <div className="portfolio-slider__text-section">
+        <h2>{currentSlide[0].textSection.title}</h2>
+        <p>{currentSlide[0].textSection.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default portfolioScreen;
