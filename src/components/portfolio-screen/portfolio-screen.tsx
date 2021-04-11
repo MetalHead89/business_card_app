2b6a634a2aad83ba45764ab8265b2913c3e273ed
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-04-11 10:50:54 +0700

    Удалён лишний код из PortfolioScreen, внедрен компонент Slider

--- src/components/portfolio-screen/portfolio-screen.tsx
+++ src/components/portfolio-screen/portfolio-screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IPortfolioScreenState } from '../../ts/interfaces';
-import SVGIcon from '../SVGIcon/SVGIcon';
+import Slider from '../slider/slider';
 import './portfolio-screen.scss';
 
 const portfolioScreen = (props: IPortfolioScreenState): JSX.Element => {
@@ -8,31 +8,7 @@ const portfolioScreen = (props: IPortfolioScreenState): JSX.Element => {
 
   return (
     <div id="portfolio" className="portfolio-screen">
-      <div className="portfolio-slider">
-        <div className="portfolio-slider__image-section">
-          <img
-            className="portfolio-slider__image"
-            src={currentSlide[0].imageSection.image}
-            alt="slide"
-          />
-        </div>
-        <div className="portfolio-slider__text-section">
-          <h2>{currentSlide[0].textSection.title}</h2>
-          <p>{currentSlide[0].textSection.description}</p>
-        </div>
-        <button className="portfolio-slider__button prev-slide-btn">
-          <SVGIcon
-            icon="left-arrow-ico"
-            class="portfolio-slider__button-icon"
-          />
-        </button>
-        <button className="portfolio-slider__button next-slide-btn">
-          <SVGIcon
-            icon="next-arrow-ico"
-            class="portfolio-slider__button-icon"
-          />
-        </button>
-      </div>
+      <Slider {...currentSlide[0]} />
     </div>
   );
 };
