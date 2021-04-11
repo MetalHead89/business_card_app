48f9ddf90806d4caf9c8255d190a41b6ab125aaf
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-04-11 18:35:34 +0700

    В ScreenReducer добавлены clickToPrevSlideBtnActionCreator, clickToNextSlideBtnActionCreator и несколько вспомогательных функций

--- src/ts/redux/portfolio-screen-reducer.ts
+++ src/ts/redux/portfolio-screen-reducer.ts
@@ -1,9 +1,13 @@
-import { IPortfolioScreenState } from '../interfaces';
+import { IAction, IPortfolioScreenState, ISlide } from '../interfaces';
+
+const CLICK_TO_PREV_SLIDE_BTN = 'CLICK-TO-PREV-SLIDE-BTN';
+const CLICK_TO_NEXT_SLIDE_BTN = 'CLICK-TO-NEXT-SLIDE-BTN';
 
 const initialState = {
   slider: {
     slides: [
       {
+        id: 1,
         active: true,
         imageSection: {
           image: '/src/images/image-stub.jpg',
@@ -14,33 +18,88 @@ const initialState = {
         },
       },
       {
+        id: 2,
         active: false,
         imageSection: {
           image: '/src/images/image-stub.jpg',
         },
         textSection: {
           title: 'Заголовок',
-          description: 'Какой-то текст с описанием работы',
+          description: 'Какой-то текст с описанием работы 2',
         },
       },
       {
+        id: 3,
         active: false,
         imageSection: {
           image: '/src/images/image-stub.jpg',
         },
         textSection: {
           title: 'Заголовок',
-          description: 'Какой-то текст с описанием работы',
+          description: 'Какой-то текст с описанием работы 3',
         },
       },
     ],
   },
 };
 
+const getNextSlideId = (id: number, state: ISlide[]): number => {
+  const currentSlideIndex = state.findIndex((item) => item.id === id);
+  return state[currentSlideIndex + 1].id || state[0].id;
+};
+
+const getPrevSlideId = (id: number, state: ISlide[]): number => {
+  const currentSlideIndex = state.findIndex((item) => item.id === id);
+  return state[currentSlideIndex - 1].id || state[-1].id;
+};
+
+const getNewSlidesState = (id: number, state: ISlide[]): ISlide[] => {
+  return state.map((item) => {
+    return item.id === id
+      ? { ...item, active: true }
+      : { ...item, active: false };
+  });
+};
+
+const clickToPrevSlideBtnActionCreator = (id: number): IAction => ({
+  type: CLICK_TO_PREV_SLIDE_BTN,
+  args: { id },
+});
+
+const clickToNextSlideBtnActionCreator = (id: number): IAction => ({
+  type: CLICK_TO_NEXT_SLIDE_BTN,
+  args: { id },
+});
+
 const portfolioScreenReducer = (
   state: IPortfolioScreenState = initialState,
+  action: IAction
 ): IPortfolioScreenState => {
-  return state;
+  switch (action.type) {
+    case CLICK_TO_PREV_SLIDE_BTN:
+      return {
+        ...state,
+        slider: {
+          slides: getNewSlidesState(
+            getPrevSlideId(action.args.id, state.slider.slides),
+            state.slider.slides
+          ),
+        },
+      };
+    case CLICK_TO_NEXT_SLIDE_BTN:
+      return {
+        ...state,
+        slider: {
+          slides: getNewSlidesState(
+            getNextSlideId(action.args.id, state.slider.slides),
+            state.slider.slides
+          ),
+        },
+      };
+    default:
+      return state;
+  }
 };
 
 export default portfolioScreenReducer;
+export { clickToPrevSlideBtnActionCreator, clickToNextSlideBtnActionCreator };
