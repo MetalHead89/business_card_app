1295b0aee89659561873a38d66d0f70bdabfccd1
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-04-11 18:39:45 +0700

    В компонент Slider добавлены события клика по кнопкам переключения слайдов

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import { ISlide } from '../../ts/interfaces';
+import { ISlider } from '../../ts/interfaces';
 import SVGIcon from '../SVGIcon/SVGIcon';
 import './slider.scss';
 
-const Slider = (props: ISlide): JSX.Element => {
+const Slider = (props: ISlider): JSX.Element => {
   return (
     <div className="slider">
       <div className="slider__image-section">
         <img
           className="slider__image"
-          src={props.imageSection.image}
+          src={props.slide.imageSection.image}
           alt="slide"
         />
       </div>
       <div className="slider__text-section">
-        <h2>{props.textSection.title}</h2>
-        <p>{props.textSection.description}</p>
+        <h2>{props.slide.textSection.title}</h2>
+        <p>{props.slide.textSection.description}</p>
       </div>
-      <button className="slider__button slider__button_left-pos">
+      <button
+        className="slider__button slider__button_left-pos"
+        onClick={() => props.clickToPrevSlideBtn(props.slide.id)}
+      >
         <SVGIcon icon="left-arrow-ico" class="slider__button-icon" />
       </button>
-      <button className="slider__button slider__button_right-pos">
+      <button
+        className="slider__button slider__button_right-pos"
+        onClick={() => props.clickToNextSlideBtn(props.slide.id)}
+      >
         <SVGIcon icon="next-arrow-ico" class="slider__button-icon" />
       </button>
     </div>
