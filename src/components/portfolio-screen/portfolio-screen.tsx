20963e6de746f4209177e4cc0900c171c2aac58e
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-04-11 18:38:27 +0700

    PortfolioScreen изменён в соответствии с интерфейсом IPortfolioScreen

--- src/components/portfolio-screen/portfolio-screen.tsx
+++ src/components/portfolio-screen/portfolio-screen.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { IPortfolioScreenState } from '../../ts/interfaces';
+import { IPortfolioScreen } from '../../ts/interfaces';
 import Slider from '../slider/slider';
 import './portfolio-screen.scss';
 
-const portfolioScreen = (props: IPortfolioScreenState): JSX.Element => {
+const portfolioScreen = (props: IPortfolioScreen): JSX.Element => {
   const currentSlide = props.slider.slides.filter((slide) => slide.active);
 
   return (
     <div id="portfolio" className="portfolio-screen">
-      <Slider {...currentSlide[0]} />
+      <Slider
+        slide={currentSlide[0]}
+        clickToPrevSlideBtn={props.clickToPrevSlideBtn}
+        clickToNextSlideBtn={props.clickToNextSlideBtn}
+      />
     </div>
   );
 };
