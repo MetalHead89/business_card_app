8b880e60606cc67190b13ede8a467039c6f19ca5
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-04-27 16:15:22 +0700

    Добавлена иконка на MainScreen

--- src/components/MainScreen/MainScreen.tsx
+++ src/components/MainScreen/MainScreen.tsx
@@ -1,9 +1,17 @@
 import './MainScreen.scss';
+import icon from '../../images/main-screen-icon.png';
 
 function MainScreen() {
   return (
     <div className="main-screen">
-      <div className="main-screen__content">Портфолио Frontend-разработчика</div>
+      <div className="main-screen__content">
+        <div className="main-screen__section">
+          <h1>Портфолио Frontend-разработчика</h1>
+        </div>
+        <div className="main-screen__section">
+          <img className="main-screen__icon" src={icon} alt="frontend icon" />
+        </div>
+      </div>
     </div>
   );
 }
