430fe4a6ffdbc81c49cb9479b5ba9f62953200bc
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-04-29 15:25:07 +0700

    Изменен фон MainScreen

--- src/components/MainScreen/MainScreen.tsx
+++ src/components/MainScreen/MainScreen.tsx
@@ -1,16 +1,13 @@
 import './MainScreen.scss';
-import icon from '../../images/main-screen-icon.png';
 
 function MainScreen() {
   return (
     <div className="main-screen">
       <div className="main-screen__content">
-        <div className="main-screen__section">
-          <h1>Портфолио Frontend-разработчика</h1>
-        </div>
-        <div className="main-screen__section">
-          <img className="main-screen__icon" src={icon} alt="frontend icon" />
-        </div>
+        <h1 className="main-screen__title">
+          ПОРТФОЛИО
+          <br /> Frontend-разработчика
+        </h1>
       </div>
     </div>
   );
