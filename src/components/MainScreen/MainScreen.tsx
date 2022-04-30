b71681337d021f670b4be6310b47490b6e2b8ff2
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-04-30 23:15:42 +0700

    Изменен стиль MainScreen

--- src/components/MainScreen/MainScreen.tsx
+++ src/components/MainScreen/MainScreen.tsx
@@ -1,13 +1,21 @@
 import './MainScreen.scss';
+import { ReactComponent as Image } from '../../images/main-screen-image.svg';
 
 function MainScreen() {
   return (
     <div className="main-screen">
       <div className="main-screen__content">
-        <h1 className="main-screen__title">
-          ПОРТФОЛИО
-          <br /> Frontend-разработчика
-        </h1>
+        <div className="main-screen__section">
+          <h1 className="main-screen__title">
+            ПОРТФОЛИО
+            <br /> Frontend-разработчика
+          </h1>
+        </div>
+        <div className="main-screen__section">
+          <div className="main-screen__image-container">
+            <Image />
+          </div>
+        </div>
       </div>
     </div>
   );
