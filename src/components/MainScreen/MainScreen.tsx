e87c28fd82152f70314646decb52f77356d0d678
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-05-03 22:20:48 +0700

    Добавлено изображение на MainScreen

--- src/components/MainScreen/MainScreen.tsx
+++ src/components/MainScreen/MainScreen.tsx
@@ -1,13 +1,22 @@
 import './MainScreen.scss';
+import { ReactComponent as Image } from '../../images/main-screen-image.svg';
 
 function MainScreen() {
   return (
     <div className="main-screen">
       <div className="main-screen__content">
-        <h1 className="main-screen__title">
-          <span className="main-screen__title_color_dark">Портфолио</span>
-          <br /> frontend_разработчика
-        </h1>
+        <div className="main-screen__title-section">
+          <h1 className="main-screen__title">
+            <span className="main-screen__title_color_dark">Портфолио</span>
+            <br /> frontend_разработчика
+          </h1>
+        </div>
+
+        <div className="main-screen__image-section">
+          <div className="main-screen__image-container">
+            <Image />
+          </div>
+        </div>
       </div>
     </div>
   );
