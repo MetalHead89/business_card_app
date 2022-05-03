439fb3d5f4073ad2dde3fe25c49e375ba1b2d890
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-05-03 19:24:13 +0700

    Изменен дизайн MainScreen

--- src/components/MainScreen/MainScreen.tsx
+++ src/components/MainScreen/MainScreen.tsx
@@ -1,20 +1,13 @@
 import './MainScreen.scss';
-import { ReactComponent as Image } from '../../images/main-screen-image.svg';
 
 function MainScreen() {
   return (
     <div className="main-screen">
       <div className="main-screen__content">
-        <div className="main-screen__section">
-          <h1 className="main-screen__title">
-            <span className='main-screen__title_color_dark'>Портфолио</span><br /> frontend_разработчика
-          </h1>
-        </div>
-        <div className="main-screen__section">
-          <div className="main-screen__image-container">
-            <Image width='100%' />
-          </div>
-        </div>
+        <h1 className="main-screen__title">
+          <span className="main-screen__title_color_dark">Портфолио</span>
+          <br /> frontend_разработчика
+        </h1>
       </div>
     </div>
   );
