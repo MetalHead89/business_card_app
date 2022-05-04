05eaf5bd5b8c8ddeb84ea89f5f0a3808ee207f83
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-05-04 10:02:17 +0700

    Добавлена анимация появления заголовка на MainScreen

--- src/components/MainScreen/MainScreen.tsx
+++ src/components/MainScreen/MainScreen.tsx
@@ -1,15 +1,32 @@
 import './MainScreen.scss';
 import { ReactComponent as Image } from '../../images/main-screen-image.svg';
+import { motion } from 'framer-motion';
+
+const animation = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    transition: { delay: 1, duration: 1 },
+    opacity: 1,
+  },
+};
 
 function MainScreen() {
   return (
     <div className="main-screen">
       <div className="main-screen__content">
         <div className="main-screen__title-section">
-          <h1 className="main-screen__title">
+          <motion.h1
+            variants={animation}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ amount: 0.5 }}
+            className="main-screen__title"
+          >
             <span className="main-screen__title_color_dark">Портфолио</span>
             <br /> frontend_разработчика
-          </h1>
+          </motion.h1>
         </div>
 
         <div className="main-screen__image-section">
