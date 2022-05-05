0811b1c336168e76234812f17e47f39fd4905afe
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-05-05 12:26:38 +0700

    Изменен контент AboutMeScreen

--- src/components/AboutMeScreen/AboutMeScreen.tsx
+++ src/components/AboutMeScreen/AboutMeScreen.tsx
@@ -1,42 +1,58 @@
-import { motion } from 'framer-motion';
-import { useAppSelector } from '../../redux/hooks';
 import './AboutMeScreen.scss';
 
 function AboutMeScreen() {
-  const animation = {
-    hidden: {
-      x: -100,
-      opacity: 0,
-    },
-    visible: (i: number) => ({
-      transition: {
-        delay: i * 0.3,
-      },
-      x: 0,
-      opacity: 1,
-    }),
-  };
-
-  const { aboutText } = useAppSelector((state) => state.aboutMeScreenReducer);
-
-  const paragraphs = aboutText.map((paragraph, i) => (
-    <motion.p custom={i} key={paragraph} variants={animation}>
-      {paragraph}
-    </motion.p>
-  ));
-
   return (
     <div className="about-me-screen">
-      <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ amount: 0.5 }}
-        className="about-me-screen__content"
-      >
-        {paragraphs}
-      </motion.div>
+      <div className="about-me-screen__content">
+        <div className="about-me-screen__text">
+          <h2>Привет! Меня зовут Виталий</h2>
+          <p>
+            Я начинающий frontend разработчик из небольшого Сибирского городка.
+            Здесь ты можешь ознакомиться с моими работами, узнать об уровне моих
+            скиллов и если возникнет необходимость - связаться со мной.
+          </p>
+        </div>
+        <div className="about-me-screen__skills">
+          
+        </div>
+      </div>
     </div>
   );
+
+  //   const animation = {
+  //     hidden: {
+  //       x: -100,
+  //       opacity: 0,
+  //     },
+  //     visible: (i: number) => ({
+  //       transition: {
+  //         delay: i * 0.3,
+  //       },
+  //       x: 0,
+  //       opacity: 1,
+  //     }),
+  //   };
+
+  //   const { aboutText } = useAppSelector((state) => state.aboutMeScreenReducer);
+
+  //   const paragraphs = aboutText.map((paragraph, i) => (
+  //     <motion.p custom={i} key={paragraph} variants={animation}>
+  //       {paragraph}
+  //     </motion.p>
+  //   ));
+
+  //   return (
+  //     <div className="about-me-screen">
+  //       <motion.div
+  //         initial="hidden"
+  //         whileInView="visible"
+  //         viewport={{ amount: 0.5 }}
+  //         className="about-me-screen__content"
+  //       >
+  //         {paragraphs}
+  //       </motion.div>
+  //     </div>
+  //   );
 }
 
 export default AboutMeScreen;
