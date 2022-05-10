c5d4f09f35a3bbc328eb3e494580f721e4ea7623
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-05-10 21:05:17 +0700

    Изменен дизайн AboutMeScreen

--- src/components/AboutMeScreen/AboutMeScreen.tsx
+++ src/components/AboutMeScreen/AboutMeScreen.tsx
@@ -15,15 +15,23 @@ function AboutMeScreen() {
   return (
     <div className="about-me-screen">
       <div className="about-me-screen__content">
-        <div className="about-me-screen__text">
-          Привет! Меня зовут{' '}
-          <span className="about-me-screen__accent">Виталий</span>. Я начинающий{' '}
-          <span className="about-me-screen__accent">frontend-разработчик</span>{' '}
-          из небольшого Сибирского городка. Здесь ты можешь ознакомиться с моими
-          работами, узнать об уровне моих скиллов и если возникнет необходимость
-          - связаться со мной.
+        <h2 className="about-me-screen__title">
+          Обо <span className="about-me-screen__accent">мне</span>
+        </h2>
+        <div className="about-me-screen__info-container">
+          <div className="about-me-screen__text">
+            Привет! Меня зовут{' '}
+            <span className="about-me-screen__accent">Виталий</span>. Я
+            начинающий{' '}
+            <span className="about-me-screen__accent">
+              frontend-разработчик
+            </span>{' '}
+            из небольшого Сибирского городка. Здесь ты можешь ознакомиться с
+            моими работами, узнать об уровне моих скиллов и если возникнет
+            необходимость - связаться со мной.
+          </div>
+          <div className="about-me-screen__info">{info}</div>
         </div>
-        <div className="about-me-screen__info">{info}</div>
       </div>
       <div className="about-me-screen__triangle">
         <SVGTriangle color="gray" />
