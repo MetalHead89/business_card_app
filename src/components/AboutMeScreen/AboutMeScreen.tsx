6a0b6ea5e6f4da758a12076d883e2c5e9c4cd621
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-05-11 15:11:46 +0700

    Добавлена анимация для элементов AboutMeScreen

--- src/components/AboutMeScreen/AboutMeScreen.tsx
+++ src/components/AboutMeScreen/AboutMeScreen.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { useAppSelector } from '../../redux/hooks';
 import AnimatedBackground from '../AnimatedBackground/AnimatedBackground';
 import InfoItem from '../InfoItem/InfoItem';
@@ -5,12 +6,29 @@ import SVGTriangle from '../SVGTriangle/SVGTriangle';
 import './AboutMeScreen.scss';
 
 function AboutMeScreen() {
+  const animation = {
+    hidden: {
+      opacity: 0,
+      y: -40,
+    },
+    visible: (custom: number) => ({
+      transition: { delay: custom * 0.3 },
+      opacity: 1,
+      y: 0,
+    }),
+  };
+
   const { infoItems } = useAppSelector((state) => state.aboutMeScreenReducer);
 
-  const info = infoItems.map((info) => (
-    <div key={info.name} className="about-me-screen__info-item">
+  const info = infoItems.map((info, index) => (
+    <motion.div
+      key={info.name}
+      className="about-me-screen__info-item"
+      variants={animation}
+      custom={index + 3}
+    >
       <InfoItem name={info.name} text={info.text} />
-    </div>
+    </motion.div>
   ));
 
   return (
@@ -20,12 +38,25 @@ function AboutMeScreen() {
       </div>
 
       <div className="about-me-screen__content">
-        <h2 className="about-me-screen__title">
+        <motion.h2
+          variants={animation}
+          custom={2}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ amount: 1, once: true }}
+          className="about-me-screen__title"
+        >
           Обо <span className="about-me-screen__accent">мне</span>
-        </h2>
-        <div className="about-me-screen__info-container">
+        </motion.h2>
+
+        <motion.div
+          className="about-me-screen__info-container"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ amount: 0.7, once: true }}
+        >
           <div className="about-me-screen__text">
-            <p>
+            <motion.p variants={animation} custom={1}>
               Привет! Меня зовут{' '}
               <span className="about-me-screen__accent">Виталий</span>. Я
               начинающий{' '}
@@ -33,14 +64,14 @@ function AboutMeScreen() {
                 frontend-разработчик
               </span>{' '}
               из небольшого Сибирского городка.
-            </p>
-            <p>
+            </motion.p>
+            <motion.p variants={animation} custom={2}>
               Здесь ты можешь ознакомиться с моими работами, узнать об уровне
               моих скиллов и если возникнет необходимость - связаться со мной.
-            </p>
+            </motion.p>
           </div>
           <div className="about-me-screen__info">{info}</div>
-        </div>
+        </motion.div>
       </div>
       <div className="about-me-screen__triangle">
         <SVGTriangle color="gray" />
