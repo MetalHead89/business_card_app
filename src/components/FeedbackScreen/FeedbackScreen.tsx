170300a4278779e2b5e5c01c00fdcd367b5e010f
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-05-24 20:36:12 +0700

    В FeedbackScreen добавлена форма с полями

--- src/components/FeedbackScreen/FeedbackScreen.tsx
+++ src/components/FeedbackScreen/FeedbackScreen.tsx
@@ -3,8 +3,34 @@ import './FeedbackScreen.scss';
 function FeedbackScreen() {
   return (
     <div className="feedback-screen">
-      <h2 className="feedback-screen__title">Обратная связь</h2>
-      <div className="feedback-screen__form"></div>
+      <div className="feedback-screen__content">
+        <h2 className="feedback-screen__title">Обратная связь</h2>
+        <form className="feedback-screen__form">
+          <input
+            className="feedback-screen__input"
+            type="text"
+            name="name"
+            placeholder="Ваше Имя *"
+          />
+          <input
+            className="feedback-screen__input"
+            type="email"
+            name="email"
+            placeholder="Email *"
+          />
+          <input
+            className="feedback-screen__input"
+            type="text"
+            name="theme"
+            placeholder="Тема обращения *"
+          />
+          <textarea
+            className="feedback-screen__input"
+            name="message"
+            placeholder="Сообщение *"
+          />
+        </form>
+      </div>
     </div>
   );
 }
