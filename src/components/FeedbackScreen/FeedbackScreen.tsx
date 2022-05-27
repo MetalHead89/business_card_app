a7166c83aeea240f002892e125a2784253bc4fa4
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-05-27 21:27:08 +0700

    Для FeedbackScreen Добавлен параллакс эффект при прокрутке

--- src/components/FeedbackScreen/FeedbackScreen.tsx
+++ src/components/FeedbackScreen/FeedbackScreen.tsx
@@ -1,40 +1,45 @@
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 import './FeedbackScreen.scss';
 
 function FeedbackScreen() {
   return (
-    <div className="feedback-screen">
-      <div className="feedback-screen__content">
-        <h2 className="feedback-screen__title">Обратная связь</h2>
-        <form className="feedback-screen__form">
-          <input
-            className="feedback-screen__input"
-            type="text"
-            name="name"
-            placeholder="Ваше Имя *"
-          />
-          <input
-            className="feedback-screen__input"
-            type="email"
-            name="email"
-            placeholder="Email *"
-          />
-          <input
-            className="feedback-screen__input"
-            type="text"
-            name="theme"
-            placeholder="Тема обращения *"
-          />
-          <textarea
-            className="feedback-screen__input"
-            name="message"
-            placeholder="Сообщение *"
-          />
-          <button className="feedback-screen__button" type="submit">
-            Отправить
-          </button>
-        </form>
-      </div>
-    </div>
+    <ParallaxProvider>
+      <Parallax speed={-40}>
+        <div className="feedback-screen">
+          <div className="feedback-screen__content">
+            <h2 className="feedback-screen__title">Обратная связь</h2>
+            <form className="feedback-screen__form">
+              <input
+                className="feedback-screen__input"
+                type="text"
+                name="name"
+                placeholder="Ваше Имя *"
+              />
+              <input
+                className="feedback-screen__input"
+                type="email"
+                name="email"
+                placeholder="Email *"
+              />
+              <input
+                className="feedback-screen__input"
+                type="text"
+                name="theme"
+                placeholder="Тема обращения *"
+              />
+              <textarea
+                className="feedback-screen__input"
+                name="message"
+                placeholder="Сообщение *"
+              />
+              <button className="feedback-screen__button" type="submit">
+                Отправить
+              </button>
+            </form>
+          </div>
+        </div>
+      </Parallax>
+    </ParallaxProvider>
   );
 }
 
